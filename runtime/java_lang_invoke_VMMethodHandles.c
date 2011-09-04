1eab2524adcf748ba970803abfbeea3e143fdf8a
Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
2011-09-04 11:39:32 +0800

    vm, runtime: Implement VMMethodHandles.findStatic()
    Signed-off-by: Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- runtime/java_lang_invoke_VMMethodHandles.c
+++ runtime/java_lang_invoke_VMMethodHandles.c
@@ -11,7 +11,40 @@
 
 jobject java_lang_invoke_VMMethodHandles_findStatic(jobject lookup, jobject refc, jobject name, jobject method_type)
 {
-	assert(0);
+	struct vm_class *vmc;
+	unsigned int i;
+        char *name_s;
+
+	vmc = vm_object_to_vm_class(refc);
+	if (!vmc)
+		return rethrow_exception();
+
+	name_s = vm_string_to_cstr(name);
+        if (!name_s)
+                return throw_oom_error();
+
+	for (i = 0; i < vmc->class->methods_count; i++) {
+		struct vm_method *vmm = &vmc->methods[i];
+		struct vm_object *mh, *vm_mh;
+
+		if (!vm_method_is_static(vmm))
+			continue;
+
+		if (strcmp(name_s, vmm->name))
+			continue;
+
+		mh = vm_object_alloc(vm_java_lang_invoke_MethodHandle);
+                if (!mh)
+                        return rethrow_exception();
+
+                vm_mh = vm_object_alloc(vm_java_lang_invoke_VMMethodHandle);
+                if (!vm_mh)
+                        return rethrow_exception();
+
+		vm_call_method(vm_java_lang_invoke_MethodHandle_init, mh, vm_mh);
+
+		return mh;
+	}
 
 	return NULL;
 }
